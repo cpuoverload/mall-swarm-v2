e440ffa2094c32a8cf35a100930662bf933734d7
cpuoverload <singhainanjifan@outlook.com>
2025-05-27 20:30:51 +0800

    test

--- mall-gateway/src/test/java/com/macro/mall/MallGatewayApplicationTests.java
+++ mall-gateway/src/test/java/com/macro/mall/MallGatewayApplicationTests.java
@@ -2,13 +2,15 @@ package com.macro.mall;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 public class MallGatewayApplicationTests {
 
     @Test
     public void contextLoads() {
+        assertTrue(true, "------------hello");
     }
 
 }
